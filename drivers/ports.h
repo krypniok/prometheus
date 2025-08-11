c9c5bba1f4d6de625a080465bdb2b0c5021f1537
krypniok <61298806+krypniok@users.noreply.github.com>
2025-08-12 01:27:46 +0200

    Improve beep timing and sound setup

--- drivers/ports.h
+++ drivers/ports.h
@@ -8,4 +8,6 @@ void port_byte_out(uint16_t port, uint8_t data);
 
 unsigned short port_word_in(uint16_t port);
 
-void port_word_out(uint16_t port, uint16_t data);
+  void port_word_out(uint16_t port, uint16_t data);
+
+void io_wait();
